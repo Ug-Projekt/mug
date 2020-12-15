412aadd716a9e1567dc30227f23759809980f6c8
Carpall <juniormondi@gmailcom>
2020-12-15 13:44:57 +0100

    adding files

---
+++ source/Debug.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>
+///  use it to print debug information; in release it will not work
+/// </summary>
+class debug
+{
+    public static void print(params string[] msg)
+    {
+#if DEBUG
+        Console.WriteLine(string.Join("", msg));
+#endif
+    }
+    public static void printif(bool condition, params string[] msg)
+    {
+#if DEBUG
+        if (condition)
+            Console.WriteLine(string.Join("", msg));
+#endif
+    }
+    public static bool askfast(char expected, params string[] msg)
+    {
+#if DEBUG
+        Console.Write(string.Join("", msg)+"? [y/..]: ");
+        var x = Console.ReadKey().KeyChar == expected;
+        Console.Write('\n');
+        return x;
+#endif
+    }
+}
---
+++ source/Main.cs
@@ -0,0 +1,5 @@
+﻿using Mug.Models;
+using System;
+
+var tokens = new CompilationUnit();
+debug.printif(debug.askfast('y', "Show TokenCollection"), tokens);
