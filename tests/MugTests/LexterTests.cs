aff3818c776f1f3b3455baab654b3da8cb9a6592
carpal <65513900+Carpall@users.noreply.github.com>
2021-02-12 12:06:27 +0100

    Some improvement
    * Made the string tests `readonly`: C# conventions prefer `readonly` over nothing
    * Spaced code, and formatted strings: C# conventions prefer formatted strings over string concatenations
    * Question: at line `34` you test the lenght using an `Assert`, so why at line `36` you don't use a `for`?
    * Inverted parameters lists and renamed them: before, you was managing `reals`(list2) as the expected tokens, but at line `74` you was passing `expected`(expectedTokens) as second parameter
    * Fixed EOF position

--- tests/MugTests/LexterTests.cs
+++ tests/MugTests/LexterTests.cs
@@ -7,9 +7,9 @@ namespace MugTests
 {
     public class Tests
     {
-        private string operation1 = "1 + 2";
-        private string variable1 = "var x = 0;";
-        private string variable2 = "var number: i32 = 5;";
+        private readonly string operation1 = "1 + 2";
+        private readonly string variable1 = "var x = 0;";
+        private readonly string variable2 = "var number: i32 = 5;";
 
         [SetUp]
         public void Setup()
@@ -30,26 +30,25 @@ namespace MugTests
         {
             MugLexer lexer = new MugLexer("test", variable1);
             lexer.Tokenize();
-            Console.WriteLine("0: " + lexer.TokenCollection[0].Value);
-            Console.WriteLine("1: " + lexer.TokenCollection[1].Value);
-            Console.WriteLine("2: " + lexer.TokenCollection[2].Value);
-            Console.WriteLine("3: " + lexer.TokenCollection[3].Value);
-            Console.WriteLine("4: " + lexer.TokenCollection[4].Value);
-            Console.WriteLine("5: " + lexer.TokenCollection[5].Value);
+            
             Assert.AreEqual(lexer.Length, 6);
+            
+            Console.WriteLine($"0: {lexer.TokenCollection[0].Value}");
+            Console.WriteLine($"1: {lexer.TokenCollection[1].Value}");
+            Console.WriteLine($"2: {lexer.TokenCollection[2].Value}");
+            Console.WriteLine($"3: {lexer.TokenCollection[3].Value}");
+            Console.WriteLine($"4: {lexer.TokenCollection[4].Value}");
+            Console.WriteLine($"5: {lexer.TokenCollection[5].Value}");
         }
 
-        public void AreListEqual(List<Token> list1, List<Token> list2)
+        public void AreListEqual(List<Token> expected, List<Token> reals)
         {
-            for(int i = 0; i < list1.Count; i++)
-            {
-                if (!list1[i].Equals(list2[i]))
-                {
-                    Assert.Fail("Assert different values. Expected: " + list1[1].Kind +
-                        ", " + list1[i].Value + ", " + list1[i].Position +". Found: " +
-                        list2[i].Kind + ", " + list2[i].Value + ", " + list2[i].Position);
-                }
-            }
+            if (reals.Count != expected.Count)
+                Assert.Fail($"Assert different lenghts:\n   - expected {expected.Count} tokens\n   - found {reals.Count} tokens");
+                
+            for(int i = 0; i < reals.Count; i++)
+                if (!reals[i].Equals(expected[i]))
+                    Assert.Fail($"Assert different values:\n   - expected: {expected[i]}\n   - found: {reals[i]}");
 
             Assert.Pass();
         }
@@ -59,19 +58,20 @@ namespace MugTests
         {
             MugLexer lexer = new MugLexer("test", variable1);
             lexer.Tokenize();
+            
             List<Token> tokens = lexer.TokenCollection;
 
-            List<Token> expectedTokens = new List<Token>
+            List<Token> expected = new List<Token>
             {
                 new Token(TokenKind.KeyVar, "var", 0..3),
                 new Token(TokenKind.Identifier, "x", 3..4),
                 new Token(TokenKind.Equal, "=", 5..6),
                 new Token(TokenKind.KeyTi32, "0", 7..8),
                 new Token(TokenKind.Colon, ";", 9..10),
-                new Token(TokenKind.EOF, "<EOF>", 11..12)
+                new Token(TokenKind.EOF, "<EOF>", 10..11)
             };
 
-            AreListEqual(tokens, expectedTokens);
+            AreListEqual(expected, tokens);
         }
     }
 }
